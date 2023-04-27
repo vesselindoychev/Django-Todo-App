80e2f2c630169f4697a37bd27b4b9a512bf8381a
Veselin Doychev <veselindoi123@gmail.com>
2023-04-27 12:49:58 +0300

    Added Task Model

--- todo_app/todo_app/base/models.py
+++ todo_app/todo_app/base/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+UserModel = get_user_model()
+
+
+class Task(models.Model):
+    TITLE_MAX_LENGTH = 50
+    TITLE_MIN_LENGTH = 2
+
+    title = models.CharField(
+        max_length=TITLE_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(TITLE_MIN_LENGTH),
+        )
+    )
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+    )
+
+    complete = models.BooleanField(
+        default=False,
+    )
+
+    create = models.DateTimeField(
+        auto_now_add=True,
+    )
+
+    user = models.ForeignKey(
+        UserModel,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('complete', )
