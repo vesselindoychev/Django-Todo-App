7beee500a680b240c75d5195389c157b2e6449c8
Veselin Doychev <veselindoi123@gmail.com>
2023-04-27 13:18:41 +0300

    Added Login url, view, template

--- todo_app/todo_app/accounts/views.py
+++ todo_app/todo_app/accounts/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import views as auth_views
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class LoginUserView(auth_views.LoginView):
+    template_name = 'accounts/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
