1b57638f6b05a354d5bada1d02090a14f7b5930e
Veselin Doychev <veselindoi123@gmail.com>
2023-04-27 16:05:33 +0300

    Added url for task-list

--- todo_app/todo_app/base/urls.py
+++ todo_app/todo_app/base/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from todo_app.base.views.generic import HomeView
+from todo_app.base.views.tasks import TaskListView
 
 urlpatterns = (
     path('', HomeView.as_view(), name='home'),
+    path('tasks-lists/', TaskListView.as_view(), name='task lists'),
 )
