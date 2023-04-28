f36a234796d1e261e77169830254fc2393fbf4c6
Veselin Doychev <veselindoi123@gmail.com>
2023-04-28 14:03:39 +0300

    Added static and mediafiles configuration

--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -104,6 +104,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
